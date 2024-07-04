34e9ffa858880d4488eba1bf41f21a276d6c9fef
Coder Nazmul Hossain <nazmulhossain3691215@gmail.com>
2024-07-04 07:51:11 +0600

    Relational and Logical Operator

---
+++ Relational and Logical Operator/index.js
@@ -0,0 +1,38 @@
+// Relational operator ->,>,>=, <,<=,==,=== ,!=,!==
+ 
+// logical operator -> && ,|| ,!
+
+let number1 =20;
+let number2 ='30';
+let number3 =40;
+// if(number1>number3){
+//     document.write("worng number1");
+// }else if(number1<number2){
+//     document.write('False Number ');
+// }else{
+//  document.write("Plase a write number");
+// }
+
+
+// if(number3>number1){
+//     document.write("The Number 1 Is True");
+// }else if(number1<number2){
+//     document.write("The Number  2  Is True");
+// }else{
+//      document.write("Plase a write number");
+// }
+
+// if(number1!=number2){
+//     document.write("First Condaton Is True");
+// }else if(number2!=number3){
+//      document.write("Secount Condaton Is True");
+// }else{
+//       document.write("Plase a write number");
+// }
+if($number1===number2){
+    document.write("True");
+}else if($number2===number3){
+     document.write("True");
+}else{
+    document.write("Plase a write number"); 
+}
---
+++ how to make temperature converter/index.js
@@ -0,0 +1,8 @@
+// var fahrenheit  =parseFloat(prompt("Enter Your Fahrenheit Number = "));
+// var celsius  =(fahrenheit-32)*5/9;
+// document.write("The Celsius Number ="+celsius);
+
+var celsius  =parseFloat(prompt("Enter Your celsius Number = "));
+var fahrenheit  =(celsius*9/5)+32;
+document.write("The fahrenheit Number ="+fahrenheit);
+ 
