1e30a7fd80c70cf7717a566e32948475375db300
Coder Nazmul Hossain <nazmulhossain3691215@gmail.com>
2024-07-04 08:31:20 +0600

    added else if  even added

---
+++ elseif switch/index.js
@@ -0,0 +1,15 @@
+// print a even number and odd number
+
+// let number =6;
+// if(number%2==0){
+//     document.write("Even Number");
+// }else{
+//      document.write("odd Number");
+// }
+
+let number =6;
+if(number%2!=0){
+    document.write("Even Number");
+}else{
+     document.write("odd Number");
+}
