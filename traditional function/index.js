b012a2db4e01e89fd87e2aeabc0a25f65a8d2253
Coder Nazmul Hossain <nazmulhossain3691215@gmail.com>
2024-07-06 14:23:15 +0600

    added traditional function

---
+++ traditional function/index.js
@@ -0,0 +1,19 @@
+
+function addition(potato,onion){
+
+  var result =potato + onion  ;
+  return result;
+
+}
+function substration(potato,onion){
+  var result =potato - onion  ;
+  return result;
+
+}
+document.write("বাজারে টোটাল টাকা খরচ হইছে ="+addition(6,6)+"<br/>");
+document.write("বাজারে টোটাল টাকা খরচ হইছে ="+addition(6,5)+"<br/>");
+document.write("বাজারে টোটাল টাকা খরচ হইছে ="+substration(6,5));
+
+
+
+
