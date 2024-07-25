80e875c8c076e75aa0b443a61a623985824ef4d0
Coder Nazmul Hossain <nazmulhossain3691215@gmail.com>
2024-07-25 23:25:52 +0600

    added event onclick

---
+++ dom/onclick_event/index.js
@@ -0,0 +1,13 @@
+// function mymessage(){
+//     document.querySelector('#id1').innerHTML ="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita provident non, molestiae delectus eos corrupti.";
+// }
+// function mymessage2(){
+//     document.querySelector('#id2').innerHTML ="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita provident non, molestiae delectus eos corrupti.";
+// }
+
+function mymessage2(){
+    document.querySelector('#picture1').src='images/nazmul.jpg';
+}
+function mymessage(){
+    document.querySelector('#picture1').src='images/logo.jpg';
+}
