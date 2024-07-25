c885e82006918e56819ed8e15d09460974f3918f
Coder Nazmul Hossain <nazmulhossain3691215@gmail.com>
2024-07-25 19:43:31 +0600

    added onclick event

---
+++ dom/query selector/index.js
@@ -0,0 +1,7 @@
+// document.querySelector('#id').innerHTML='This query selector';
+// document.querySelector('.elementbyid').innerHTML='This ';
+// document.querySelector(' li a').innerHTML='yes';
+
+// document.querySelector('.my_div a').innerHTML='No';
+
+document.querySelectorAll('p')[0].innerHTML='Nazmul Hossain';
